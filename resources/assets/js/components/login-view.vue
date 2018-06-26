7f694f733ce9f1fc158dc33a5a74239b84aeb8d9
keyproco <keyproco.rd@gmail.com>
2018-06-26 16:24:09 +0200

    create login-view component with a dropdown display, add csrf field to the form and fit it in the navigation bar

---
+++ resources/assets/js/components/login-view.vue
@@ -0,0 +1,52 @@
+<template>
+                <b-dropdown position="is-bottom-left">
+                    <a class="navbar-item" slot="trigger">
+                        <span>Login</span>
+                        <b-icon icon="menu-down"></b-icon>
+                    </a>
+
+                    <b-dropdown-item custom paddingless>
+                        <form  method="POST" action="/login">
+                            <input type="hidden" name="_token" :value="csrf">
+                            <div class="modal-card" style="width:300px;">
+                                <section class="modal-card-body">
+                                    <b-field label="Email">
+                                        <b-input
+                                            name="email"
+                                            type="email"
+                                            placeholder="Your email">
+                                        </b-input>
+                                    </b-field>
+
+                                    <b-field label="Password">
+                                        <b-input
+                                            name="password"
+                                            type="password"
+                                            password-reveal
+                                            placeholder="Your password"
+                                            required>
+                                        </b-input>
+                                    </b-field>
+                                    <b-checkbox>Se souvenir de moi</b-checkbox>
+                                </section>
+                                <footer class="modal-card-foot">
+                                    <button class="button is-primary">Login</button>
+                                </footer>
+                            </div>
+                        </form>
+                    </b-dropdown-item>
+                </b-dropdown>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                csrf : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+            }
+        },
+        mounted() {
+
+        }
+    }
+</script>
