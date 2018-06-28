d0f0dfc8945f76feb1ca9c515aaafafa734bdc34
keyproco <keyproco.rd@gmail.com>
2018-06-29 00:36:35 +0200

    display the users

--- resources/views/admins/users/index.blade.php
+++ resources/views/admins/users/index.blade.php
@@ -2,5 +2,33 @@
 
 
 @section('content')
-Hello Users
+<table class="table is-center">
+  <thead>
+    <tr>
+      <th><abbr title="ID">ID</abbr></th>
+      <th>Nom</th>
+      <th><abbr title="Mail">Adresse mail</abbr></th>
+      <th><abbr title="Creation-Date">Date de création</abbr></th>
+      <th><abbr title="Modification-Date">Date de modification</abbr></th>
+       <th><abbr title="Update-date">Action</abbr></th>
+    </tr>
+  </thead>
+  <tfoot>
+    <tr>
+      <th><abbr title="Users">{{ $number }}</abbr></th>
+    </tr>
+  </tfoot>
+   	@foreach($users as $user)
+  <tbody>
+    <tr>
+      <th>{{ $user->id }}</th>
+      <td>{{ $user->name }}</td>
+      <td>{{ $user->email }}</td>
+      <td>{{ $user->created_at }}</td>
+      <td>{{ $user->updated_at }}</td>
+      </td>
+    </tr>
+  </tbody>
+       @endforeach
+</table>
 @endsection
