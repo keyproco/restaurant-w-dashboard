1c02a75d751749cc5865e9d21e59cae961d30232
keyproco <keyproco.rd@gmail.com>
2018-06-29 15:47:02 +0200

    add pagination, buttons to show users, update, delete

--- resources/views/admins/users/index.blade.php
+++ resources/views/admins/users/index.blade.php
@@ -2,29 +2,38 @@
 
 
 @section('content')
+<div class="card">
+	<div class="card-content">
 <table class="table is-center">
   <thead>
     <tr>
-      <th><abbr title="ID">ID</abbr></th>
+      <th>ID</th>
       <th>Nom</th>
-      <th><abbr title="Mail">Adresse mail</abbr></th>
-      <th><abbr title="Creation-Date">Date de création</abbr></th>
-      <th><abbr title="Modification-Date">Date de modification</abbr></th>
-       <th><abbr title="Update-date">Action</abbr></th>
+      <th>Adresse mail</th>
+      <th>Date de création</th>
+      <th>Date de modification</th>
+       <th>Actions</th>
     </tr>
   </thead>
-
-   	@foreach($users as $user)
   <tbody>
+    	@foreach($users as $user)
     <tr>
       <th>{{ $user->id }}</th>
-      <td>{{ $user->name }}</td>
+      <td><a href="{{ route('users.show', $user->id) }}">{{ $user->name }}</a></td>
       <td>{{ $user->email }}</td>
-      <td>{{ $user->created_at }}</td>
-      <td>{{ $user->updated_at }}</td>
+      <td>{{ $user->created_at->toFormattedDateString() }}</td>
+      <td>{{ $user->updated_at->toFormattedDateString() }}</td>
+      <td><a href="{{ route('users.edit', $user) }}" class="button is-success">Modifier</a>
+<a href="{{ route('users.show', $user) }}" class="button is-info">Afficher</a>
+<a href="{{ route('users.destroy', $user) }}" class="button is-danger">Supprimer</a></td>
       </td>
     </tr>
+           @endforeach
   </tbody>
-       @endforeach
-</table>
+</table>		
+
+
+{{ $users->links('vendor.pagination.default') }}
+</div>
+
 @endsection
