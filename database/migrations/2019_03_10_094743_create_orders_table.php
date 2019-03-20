a2466046d437a4f40989afe4bebdf095833f0823
keyproco <keyproco.rd@gmail.com>
2019-03-20 13:55:17 +0100

    add new columns to orders table

--- database/migrations/2019_03_10_094743_create_orders_table.php
+++ database/migrations/2019_03_10_094743_create_orders_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateOrdersTable extends Migration
 {
@@ -16,11 +16,12 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('user_id');
-            $table->unsignedInteger('delivery_id');
-            $table->integer('payment_type');
+            $table->unsignedInteger('adress_id');
             $table->boolean('confirmed')->default(false);
+            $table->boolean('accepted')->default(false);
             $table->integer('status')->default(0);
             $table->integer('total')->default(0);
+            $table->boolean('paid')->default(0);
             $table->timestamps();
         });
     }
