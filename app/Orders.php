4c5f6e6a49d2e05d8b691aed9ee5afb723b7ffe3
Oussama Keddar <oussama.keddar@epitech.eu>
2019-03-20 15:17:25 +0100

    Setting contants to Order Model,set a status when confirmed to PENDING.

--- app/Orders.php
+++ app/Orders.php
@@ -6,16 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Orders extends Model
 {
+    const PENDING = -1;
+    const IN_PROGRESS = 1;
+    const PREPARED = 2;
+
     protected $fillable = ['user_id', 'confirmed', 'delivery_id', 'payment_type'];
 
-    public function products() {
+    public function products()
+    {
         return $this->belongsToMany(Product::class)->withPivot('quantity');
     }
 
-    public function user() {
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
-    public function adress() {
+    public function adress()
+    {
         return $this->belongsTo(Adress::class, 'delivery_id');
     }
 }
