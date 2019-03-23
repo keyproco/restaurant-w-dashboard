914d92e9429c2b163a57c08438e8c0c5c1bce238
keyproco <keyproco.rd@gmail.com>
2019-03-23 04:54:03 +0100

    create status component, needs to emit data to parent to change status value

---
+++ resources/assets/js/components/Status.vue
@@ -0,0 +1,39 @@
+
+      
+    <template>
+  <div>
+    <b-field>
+      <b-radio-button v-model="statusModel" native-value="1" type="is-warning">
+        <b-icon icon="clock"></b-icon>
+        <span>En attente</span>
+      </b-radio-button>
+
+      <b-radio-button v-model="statusModel" native-value="2" type="is-info">
+        <b-icon icon="angle-double-right"></b-icon>
+        <span>Préparation</span>
+      </b-radio-button>
+
+      <b-radio-button v-model="statusModel" native-value="3">
+        <b-icon icon="check"></b-icon>Livraison
+      </b-radio-button>
+    </b-field>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      statusModel: "1"
+    };
+  },
+  props: ["status"],
+  methods: {},
+  mounted() {},
+  updated() {
+    console.log("Status from parent", this.status);
+
+    console.log("status changed To", this.statusModel);
+  }
+};
+</script>
